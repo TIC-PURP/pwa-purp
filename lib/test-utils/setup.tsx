2761c2a40987e76b327caa070bb42d883fb9d41c
TIC-PURP <purpsadecv@gmail.com>
2025-08-08 22:25:42 -0700

    Commit 17

---
+++ lib/test-utils/setup.tsx
@@ -0,0 +1,20 @@
+// lib/test-utils/setup.tsx
+import type { ReactElement, ReactNode } from "react"
+import { render, type RenderOptions } from "@testing-library/react"
+import { Provider } from "react-redux"
+import { store } from "@/lib/store"
+
+type AllProvidersProps = { children: ReactNode }
+
+function AllProviders({ children }: AllProvidersProps) {
+  return <Provider store={store}>{children}</Provider>
+}
+
+const customRender = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, "wrapper">
+) => render(ui, { wrapper: AllProviders, ...options })
+
+// Re-exporta todo de RTL y expone nuestro render custom
+export * from "@testing-library/react"
+export { customRender as render }
